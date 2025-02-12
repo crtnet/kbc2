8bb5be570dc58e596eb9b0464bff123a614850fc
crtnet <crtnet@hotmail.com>
2025-02-12 07:24:39 -0300

    Problema BookController

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
-import BookController from '../controllers/bookController';
+import BookController from '../controllers/BookController';
 
 const router = Router();
 
 // Rotas de livros
-router.post('/books', BookController.createBook);
-router.get('/books', BookController.getAllBooks);
+router.get('/books', BookController.getBooks);
 router.get('/books/:id', BookController.getBook);
+router.post('/books', BookController.createBook);
 router.put('/books/:id', BookController.updateBook);
 router.delete('/books/:id', BookController.deleteBook);
-router.post('/books/:id/generate', BookController.generateBook);
 
 export default router;
